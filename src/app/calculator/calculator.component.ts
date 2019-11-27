d51adb04a3e321f8e05cb33e0907c86c0a63166f
Jens Bäckvall <jens@entropyfox.com>
2019-11-27 09:37:54 +0100

    update calculator component getting ready for adding actual functionality

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-calculator',
-  templateUrl: 'calculator.component.html'
+  templateUrl: './calculator.component.html',
+  styleUrls: ['./calculator.component.html.scss']
 })
 
 export class CalculatorComponent implements OnInit {
