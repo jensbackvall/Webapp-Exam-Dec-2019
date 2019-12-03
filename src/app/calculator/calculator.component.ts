c15bfc20ad430ec5f05a93b5a6bac8af1db51a7f
pokealai <poxx0003@stud.kea.dk>
2019-12-03 09:35:43 +0100

    added selection sort

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -10,4 +10,26 @@ export class CalculatorComponent implements OnInit {
   constructor() { }
 
   ngOnInit() { }
+
+  quicksort(arr) {
+    for (let i = 0; i < arr.length - 1; i++) {
+      let smallest = arr[i];
+      if (isNaN(smallest)) {
+        arr[i] = 'ø';
+        continue;
+      }
+      for (let j = i + 1; j < arr.length; j++) {
+        const compareTo = arr[j];
+        if (compareTo < smallest) {
+          smallest = compareTo;
+        } else {
+          console.log(smallest + ' is now smaller than ' + compareTo)
+        }
+      }
+      const temp = arr[smallest];
+      arr[smallest] = arr[i];
+      arr[i] = temp;
+    }
+    return arr;
+  }
 }
